e5ccd5e70b1c1e5f60fc1c83490b5ef54d078bd2
ProbMoaz <nabelnadel21@gmail.com>
2022-08-03 12:31:04 +0200

    update 1

--- index.php
+++ index.php
@@ -1,32 +1,3 @@
-<?php
-function calculator()
-    {
-        $a=readline("enter the op: ");
-        $b=(int)readline("enter the Fnum: ");
-        $c=(int)readline("enter the Snum: ");
-
-        switch($a)
-                {
-                    case "+":
-                        echo $b+$c;
-                        break ;
-                    
-                    case "-":
-                        echo $b-$c;
-                        break ;
-                    
-                    case "*":
-                        echo $b*$c;
-                        break ;     
-                    case "/":
-                        echo $b/$c;
-                        break ;         
-                }
-    }
-
- calculator();  
-
-?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -37,6 +8,61 @@ function calculator()
     <title>Web Skills</title>
 </head>
 <body>
+<form>
+
+<div class="calc">
+<input type="text" name="fnum" placeholder="First number">
+<input type="text" name="snum" placeholder="Second number">
+<br>
+<select name="op">
+    <option value="none">none</option>
+    <option value="add">+</option>
+    <option value="subtract">-</option>
+    <option value="multiply">*</option>
+    <option value="divide">/</option>
+</select>
+<button type="submit" name="submit" value="submit">Calculate</button>
+<br>
+</form></div>
+<?php
+echo "The Answer is: ";
+
+function calculator()
+    {
+        if(isset($_GET['submit']))
+        {
+            $b = $_GET['fnum'];
+            $c =$_GET['snum'];
+            $op = $_GET["op"];
+            switch($op)
+            {
+                case "none":
+                    echo "please use an operation";
+                    break;                        
+                case "add":
+                    echo $b+$c;
+                    break ;
+                
+                case "subtract":
+                    echo $b-$c;
+                    break ;
+                
+                case "multiply":
+                    echo $b*$c;
+                    break ;     
+                case "divide":
+                    echo $b/$c;
+                    break ;         
+            }
+        }
+
+    }
+
+ calculator();  
+
+?>
+
+
 
 
     
